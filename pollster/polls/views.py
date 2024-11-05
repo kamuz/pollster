679ab0e10ab40983c6ce81e022876301841f65cf
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-11-05 19:12:40 +0200

    Setup urls and views for index pollster page

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Question, Choice
 
-# Create your views here.
+# Get and display questions
+def index(request):
+	return render(request, 'polls/index.html')
