8ca34524f8cf2204c13ce9b3a4dc1dc5124bb5fe
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-11-05 23:36:20 +0200

    Polls functionality

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Question, Choice
 
 # Get and display questions
 def index(request):
-	return render(request, 'polls/index.html')
+    questions = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'questions': questions
+    }
+    return render(request, 'polls/index.html', context)
+
+# Show specific question and choices
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Qestion.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+# Get question and display results
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+# Vote for a question choice
+def vote(request, question_id):
+    # return print(request.POST['choice'])
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    # Don't selected choice
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    # Always return an HttpResponseRedirect after successfully dealing with POST data.
+    # This prevents data from being posted twice if a user hits the back button
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
