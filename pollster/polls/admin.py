17621c20cef1f29800e2f76bddcc655209a1b7b5
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-11-05 18:22:46 +0200

    TabularInline for choices

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}), ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
